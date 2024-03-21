84e01a3e22383bedd2bde1baa56a85c559f7a47c
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-03-21 20:10:22 +0100

    seting values for chart

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 'use strict';
 
-const dayName = document.querySelectorAll('.day-name');
-const dayAmount = document.querySelectorAll('.day__amount');
-const dayChart = document.querySelectorAll('.day__amount-chart');
+const day = document.querySelectorAll('.day__box');
 
 const URL = './data.json';
 
 const chartData = async function () {
-  const res = await fetch(URL);
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log(data);
+
+    weekChart(data);
+  } catch {
+    console.error('error????? :(');
+  }
 };
 
 chartData();
+
+const weekChart = function (d) {
+  day.forEach(function (el, i) {
+    const dayAmount = el.querySelector('.day__amount');
+    const dayChart = el.querySelector('.day__amount-chart');
+    const dayName = el.querySelector('.day-name');
+
+    dayChart.style.height = `${d[i].amount / 4}rem`;
+    dayAmount.textContent = d[i].amount;
+    dayName.textContent = d[i].day;
+  });
+};
+
+const html = `
+<li class="day__box">
+            <p class="day__amount">${data.amount}</p>
+            <div class="day__amount-chart">&nbsp;</div>
+            <p class="day-name text">${data.day}</p>
+          </li>
+`;
