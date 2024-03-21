cc5c3bd67e7d55564e01494b416eb03a9eae8f19
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-03-21 18:18:42 +0100

    js first commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const dayName = document.querySelectorAll('.day-name');
+const dayAmount = document.querySelectorAll('.day__amount');
+const dayChart = document.querySelectorAll('.day__amount-chart');
+
+const URL = './data.json';
+
+const chartData = async function () {
+  const res = await fetch(URL);
+  const data = await res.json();
+  console.log(data);
+};
+
+chartData();
